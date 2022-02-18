28054133ab3846e6c837a8359ed8fdc97a7aaa3e
PEDAGO\m.marsili <m.marsili22@gmail.com>
2022-02-18 13:48:46 +0100

    index with 2 forms

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registrazione</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <form method="post" action="/php/register.php">
+            <h1>Formulaire d'enregistrement</h1>
+            <input type="email" id="email" placeholder="Email*" name="email" required>
+            <input type="password" id="password" placeholder="Password*" name="password" required>
+            <button type="submit" name="register">Sign up</button>
+        </form>
+        <form method="post" action="/php/login.php">
+            <h1>Formulaire de connexion</h1>
+            <input type="email" id="email" placeholder="Email*" name="email" required>
+            <input type="password" id="password" placeholder="Password*" name="password" required>
+            <button type="submit" name="login">Sign in</button>
+        </form>
+    </body>
+</html>
